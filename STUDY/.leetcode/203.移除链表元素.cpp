ba6957d3a75bc0d3c123bb06109afee862149d8f
Hikari <18520709184@163.com>
2026-03-25 09:52:54 +0800

    练习

--- STUDY/.leetcode/203.移除链表元素.cpp
+++ STUDY/.leetcode/203.移除链表元素.cpp
@@ -19,34 +19,61 @@ class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val)
     {
-        //头结点等于val时
-        while(head != NULL && head -> val == val)
+        ListNode* DummyHead = new ListNode(0); //创建虚拟头结点
+        DummyHead -> next = head;
+        ListNode* cur = DummyHead;
+        while(cur -> next != NULL)
         {
-            ListNode* tmp = head;
-            head = head -> next;
-            delete tmp; //一定要先指向下一个结点再删除！否则就无法找到下一个了
-        }
-
-        //非头结点等于val时
-        ListNode* cur = head; //cur为current，表示当前结点
-        while(cur != NULL && cur -> next != NULL)
-        {
-            if(cur -> next -> val == val) //head所在结点不为val，所以直接判断下一个结点的val
+            if(cur -> next -> val == val)
             {
                 ListNode* tmp = cur -> next;
-                cur -> next = cur -> next -> next; //让下一个结点指向下下个结点
+                cur -> next = tmp -> next;
                 delete tmp;
-            } 
-            else
+            }
+            else //不要忘记不符合时要指向next！！否则会陷入死循环TvT
             {
                 cur = cur -> next;
             }
         }
-
+        head = DummyHead -> next;
+        delete DummyHead; //记得释放内存
         return head;
     }
 };
 // @lc code=end
 
 //一刷：没接触过用结构体实现，仅仅学过数组模拟链表，是照着题解边打边理解的
+// class Solution {
+// public:
+//     ListNode* removeElements(ListNode* head, int val)
+//     {
+//         //头结点等于val时
+//         while(head != NULL && head -> val == val) //用while不用if避免连续多个头结点都为val
+//         {
+//             ListNode* tmp = head;
+//             head = head -> next;
+//             delete tmp; //一定要先指向下一个结点再删除！否则就无法找到下一个了
+//         }
 
+//         //非头结点等于val时
+//         ListNode* cur = head; //cur为current，表示当前结点
+//         while(cur != NULL && cur -> next != NULL)
+//         //接下来要操作cur->next，避免为空；同时也要取cur->的值，避免为空
+//         {
+//             if(cur -> next -> val == val) //head所在结点不为val，所以直接判断下一个结点的val
+//             {
+//                 ListNode* tmp = cur -> next;
+//                 cur -> next = cur -> next -> next; //让下一个结点指向下下个结点
+//                 delete tmp;
+//             } 
+//             else
+//             {
+//                 cur = cur -> next;
+//             }
+//         }
+
+//         return head;
+//     }
+// };
+
+//二刷：试了下虚拟头结点，不用分类讨论真不错！
