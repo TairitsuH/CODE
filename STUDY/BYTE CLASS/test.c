0234f5d4441452adbea55111ad6ff448c8ed2beb
Hikari <tairitsu_h@qq.com>
2026-03-28 17:51:06 +0800

    test

---
+++ STUDY/BYTE CLASS/test.c
@@ -0,0 +1,33 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 0;
+//     int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//     for(i=0; i<=12; i++)
+//     {
+//         arr[i] = 0;
+//         printf("hello bit\n");
+//     }
+//     return 0;
+// }
+//死循环案例
+
+// #include<stdio.h>
+// int main()
+// {
+//     int money = 20;
+//     int bo = 0;
+//     int cnt = 0;
+//     while(money--)
+//     {
+//         cnt++;
+//         bo++;
+//         if(bo == 2)
+//         {
+//             bo--;
+//             cnt++;
+//         }
+//     }
+//     printf("%d", cnt);
+//     return 0;
+// }
