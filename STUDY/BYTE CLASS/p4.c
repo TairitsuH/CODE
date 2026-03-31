1914abc7da1c1d26bd3c09f4fcd7ad58b01a2d9f
Hikari <tairitsu_h@qq.com>
2026-03-31 22:22:43 +0800

    练习

---
+++ STUDY/BYTE CLASS/p4.c
@@ -0,0 +1,14 @@
+// int main()
+// {
+//     char ch = 'x';
+//     char* pc = &ch;
+//     *pc = 'x';
+//     return 0;
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     const char* ps = "hello world";
+//     printf("%s\n", ps);
+//     return 0;
+// }
