352ef22c76f7dcd74f95f2c37cd6b94471c51f5c
Hikari <tairitsu_h@qq.com>
2026-03-31 10:43:17 +0800

    练习

--- STUDY/.leetcode/21.合并两个有序链表.cpp
+++ STUDY/.leetcode/21.合并两个有序链表.cpp
@@ -11,44 +11,60 @@
  *     struct ListNode *next;
  * };
  */
+
+typedef struct ListNode LTNode;
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
 {
+    //判空
     if(list1 == NULL) return list2;
     if(list2 == NULL) return list1;
 
-    struct ListNode* p1 = list1;
-    struct ListNode* p2 = list2;
-    struct ListNode* p3 = NULL;
+    LTNode* p1 = list1;
+    LTNode* p2 = list2;
+    LTNode* begin = NULL;
+    LTNode* end = NULL;
 
-    int flag = 0;
-    struct ListNode* newhead;
-    while(p1 != NULL && p2 != NULL)
+    //p1, p2均不为空
+    while(p1 && p2)
     {
         if(p1->val < p2->val)
         {
-            p3 = p1;
-            p3 = p3->next;
-            p1 = p1->next;
+            if(begin == NULL) begin = end = p1;
+            else 
+            {
+                end->next = p1;
+                end = end->next;
+            }
 
+            p1 = p1->next;
         }
         else
         {
-            p3 = p2;
-            p3 = p3->next;
+            if(begin == NULL) begin = end = p2;
+            else
+            {
+                end->next = p2;
+                end = end->next;
+            }
+            
             p2 = p2->next;
         }
-
-        if(flag == 0)
-        {
-            flag = 1;
-            newhead = p3;
-        } 
     }
-    
-    if(p2 != NULL) p3 = p2;
-    if(p1 != NULL) p3 = p1;
 
-    return newhead;
+    if(p1)
+    {
+        end->next = p1;
+    }
+    else
+    {
+        end->next = p2;
+    }
+
+    return begin;
+   
 }
 // @lc code=end
+
+//一刷：在原链表上操作很复杂，故直接新建链表。
+//很多需要注意的细节，比如在合并前需要对数组判空，设置两个指针分别指向两个链表的开头，再新建两个指针指向新建链表的头尾。
 
