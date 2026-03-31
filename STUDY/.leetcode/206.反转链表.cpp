d305053437c1c1a31ea9482b22e40d56e2d34d47
Hikari <tairitsu_h@qq.com>
2026-03-31 19:19:40 +0800

    练习

--- STUDY/.leetcode/206.反转链表.cpp
+++ STUDY/.leetcode/206.反转链表.cpp
@@ -5,12 +5,14 @@
  *     struct ListNode *next;
  * };
  */
+typedef struct ListNode LTNode;
 struct ListNode* reverseList(struct ListNode* head)
 {
     if(head == NULL) return head;
-    struct ListNode* n1 = NULL;
-    struct ListNode* n2 = head;
-    struct ListNode* n3 = n2->next;
+
+    LTNode *n1 = NULL;
+    LTNode *n2 = head;
+    LTNode *n3 = n2->next;
 
     while(n2)
     {
@@ -19,5 +21,28 @@ struct ListNode* reverseList(struct ListNode* head)
         n2 = n3;
         if(n3 != NULL) n3 = n3->next;
     }
+
     return n1;
 }
+
+
+
+//二刷
+//一刷：需要注意最后一次循环中当n3为NULL时不可再次解引用。
+// struct ListNode* reverseList(struct ListNode* head)
+// {
+//     if(head == NULL) return head;
+//     struct ListNode* n1 = NULL;
+//     struct ListNode* n2 = head;
+//     struct ListNode* n3 = n2->next;
+
+//     while(n2)
+//     {
+//         n2->next = n1;
+//         n1 = n2;
+//         n2 = n3;
+//         if(n3 != NULL) n3 = n3->next;
+//     }
+//     return n1;
+// }
+
