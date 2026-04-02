bfff3424d6b2404d69e420c4427153407fd13d02
Hikari <tairitsu_h@qq.com>
2026-04-02 17:45:41 +0800

    练习

--- STUDY/.leetcode/160.相交链表.c
+++ STUDY/.leetcode/160.相交链表.c
@@ -15,55 +15,96 @@
 typedef struct ListNode LTNode;
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB)
 {
-    LTNode* ha = headA;
-    LTNode* hb = headB;
-
-    int cna = 0;
-    int cnb = 0;
-    while(ha)
+    LTNode* cura = headA;
+    LTNode* curb = headB;
+    int la = 1, lb = 1;
+    while(cura->next)
     {
-        ha = ha->next;
-        cna++;
+        cura = cura->next;
+        la++;
     }
-    while(hb)
+    while(curb->next)
     {
-        hb = hb->next;
-        cnb++;
-    }
-    ha = headA;
-    hb = headB;
-    int n = abs(cna-cnb);
-    while(cnb > cna)
-    {
-        hb = hb->next;
-        cnb--;
-    }
-    while(cna > cnb)
-    {
-        ha = ha->next;
-        cna--;
+        curb = curb->next;
+        lb++;
     }
 
-    LTNode* tmp;
-    while(ha && hb)
+    //不相交
+    if(cura != curb) return NULL;
+
+    int n = abs(la - lb);
+    LTNode* long1 = headA;
+    LTNode* short1 = headB;
+    if(la < lb)
     {
-        if(ha == hb) tmp = ha;
-        while(ha == hb)
-        {
-            if(ha == NULL) return tmp;
-            ha = ha->next;
-            hb = hb->next;
-        }
-        tmp = NULL;
-        ha = ha->next;
-        hb = hb->next;
+        long1 = headB;
+        short1 = headA;
     }
-    return tmp;
+    while(n--)
+    {
+        long1 = long1->next;
+    }
+
+    while(long1 != short1)
+    {
+        long1 = long1->next;
+        short1 = short1->next;
+    }
+    return long1;
 }
 // @lc code=end
 
+//二刷：用假设法解决较长链表后移问题
 //一刷：错误思路：两个指针分别从两个链表头节点开始遍历，直到遇到相同节点，此时进入第二个循环，判断后续节点是否相同，若不同则重新返回第一循环。
 //不对不对，链表的长度不一定相同！这个思路算是报废了
 //新错误思路：链表全部逆置，然后再从新的头结点比较？ 没招了，还是不行，题目要求是链表必须要求原结构TvT
 //反思：有长度差的链表好像都可以通过计算长度差，让较长链表的指针移动这个长度。这样就可以进行比较了
 //易错点：注意比较的是地址不是值，相交的含义是节点位于同一块内存
+// typedef struct ListNode LTNode;
+// struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB)
+// {
+//     LTNode* ha = headA;
+//     LTNode* hb = headB;
+
+//     int cna = 0;
+//     int cnb = 0;
+//     while(ha)
+//     {
+//         ha = ha->next;
+//         cna++;
+//     }
+//     while(hb)
+//     {
+//         hb = hb->next;
+//         cnb++;
+//     }
+//     ha = headA;
+//     hb = headB;
+//     int n = abs(cna-cnb);
+//     while(cnb > cna)
+//     {
+//         hb = hb->next;
+//         cnb--;
+//     }
+//     while(cna > cnb)
+//     {
+//         ha = ha->next;
+//         cna--;
+//     }
+
+//     LTNode* tmp;
+//     while(ha && hb)
+//     {
+//         if(ha == hb) tmp = ha;
+//         while(ha == hb)
+//         {
+//             if(ha == NULL) return tmp;
+//             ha = ha->next;
+//             hb = hb->next;
+//         }
+//         tmp = NULL;
+//         ha = ha->next;
+//         hb = hb->next;
+//     }
+//     return tmp;
+// }
