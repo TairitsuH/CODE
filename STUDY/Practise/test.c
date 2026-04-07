3fb1b60dedc8c1a9832f48b7c5e3f9373a7617d4
Hikari <tairitsu_h@qq.com>
2026-04-07 21:09:03 +0800

    测试栈帧

--- STUDY/Practise/test.c
+++ STUDY/Practise/test.c
@@ -57,3 +57,22 @@
     
 //     return 0;
 // }
+
+#include<stdio.h>
+
+int Add(int x, int y)
+{
+    return x + y;
+}
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 0;
+
+    c = Add(a, b);
+
+    printf("%d", c);
+    return 0;
+}
