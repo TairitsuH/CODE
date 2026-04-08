34b7aa4fd66dc9be563564548ca1f5fa5d30392e
Hikari <tairitsu_h@qq.com>
2026-04-08 15:41:54 +0800

    练习

--- STUDY/.leetcode/622.设计循环队列.c
+++ STUDY/.leetcode/622.设计循环队列.c
@@ -9,40 +9,75 @@
 
 
 typedef struct {
-    
+    int* a;
+    int head; //队头元素下标
+    int tail; //队尾元素的下一个位置下标
+    int k;
 } MyCircularQueue;
 
 
-MyCircularQueue* myCircularQueueCreate(int k) {
-    
+MyCircularQueue* myCircularQueueCreate(int k)
+{
+    MyCircularQueue* obj = (MyCircularQueue*)malloc(sizeof(MyCircularQueue));
+    obj->a = (int*)malloc(sizeof(int)*(k+1));
+    obj->head = 0;
+    obj->tail = 0;
+    obj->k = k;
+    return obj;
 }
 
-bool myCircularQueueEnQueue(MyCircularQueue* obj, int value) {
-    
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value)
+{
+    if(obj->head == (obj->tail+1) % (obj->k+1))
+    return false;
+
+    obj->a[obj->tail] = value;
+    obj->tail++;
+    obj->tail = obj->tail % (obj->k + 1);
+    return true;
 }
 
-bool myCircularQueueDeQueue(MyCircularQueue* obj) {
+bool myCircularQueueDeQueue(MyCircularQueue* obj)
+{
+    if(obj->head == obj->tail)
+    return false;
     
+    obj->head == obj->k ? obj->head = 0 : obj->head++;
+    return true; 
 }
 
-int myCircularQueueFront(MyCircularQueue* obj) {
-    
+int myCircularQueueFront(MyCircularQueue* obj)
+{
+    if(obj->head == obj->tail)
+    return -1;
+
+    return obj->a[obj->head];
 }
 
-int myCircularQueueRear(MyCircularQueue* obj) {
-    
+int myCircularQueueRear(MyCircularQueue* obj)
+{
+    if(obj->head == obj->tail)
+    return -1;
+
+    return obj->tail == 0 ? obj->a[obj->k] : obj->a[obj->tail-1];
 }
 
-bool myCircularQueueIsEmpty(MyCircularQueue* obj) {
-    
+bool myCircularQueueIsEmpty(MyCircularQueue* obj)
+{
+    return obj->head == obj->tail;
 }
 
-bool myCircularQueueIsFull(MyCircularQueue* obj) {
-    
+bool myCircularQueueIsFull(MyCircularQueue* obj)
+{
+    return (obj->tail + 1) % (obj->k + 1) == obj->head;    
 }
 
-void myCircularQueueFree(MyCircularQueue* obj) {
-    
+void myCircularQueueFree(MyCircularQueue* obj)
+{
+    free(obj->a);
+    obj->a = NULL;
+    free(obj);
+    obj = NULL;
 }
 
 /**
@@ -63,4 +98,6 @@ void myCircularQueueFree(MyCircularQueue* obj) {
  * myCircularQueueFree(obj);
 */
 // @lc code=end
+
+//一刷：好多需要注意的细节，比如新建的是大小为k+1的数组，但最大下标是k，一不小心就会越界TvT还有取余运算等等，看来画图还是挺重要的
 
