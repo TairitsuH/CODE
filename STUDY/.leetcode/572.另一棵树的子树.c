5a80a7721c35e17ef1dc01576450915164cf79d9
Hikari <tairitsu_h@qq.com>
2026-04-09 13:38:46 +0800

    练习

---
+++ STUDY/.leetcode/572.另一棵树的子树.c
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=c
+ *
+ * [572] 另一棵树的子树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+typedef struct TreeNode TNode;
+bool isSameTree(TNode* root, TNode* subroot)
+{
+    if(!root && !subroot) return true;
+
+    if(!root || !subroot) return false;
+
+    if(root->val != subroot->val) return false;
+
+    return isSameTree(root->left, subroot->left) && isSameTree(root->right, subroot->right);
+}
+
+bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot)
+{
+    if(!root)
+    {
+        return false;
+    }
+
+    if(root->val == subRoot->val && isSameTree(root, subRoot))
+    {
+        return true;
+    }
+
+    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+
+}
+// @lc code=end
+
+//一刷：递归的思路还是要加强TvT。本质上是遍历树，找出所有子树与subroot进行比较，最后只要有true表示找到了子树（因为返回是或关系
+
