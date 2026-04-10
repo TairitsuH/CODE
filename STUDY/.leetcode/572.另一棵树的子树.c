2b1ef398e67dc27991e63a2d912a154f5444e04a
Hikari <tairitsu_h@qq.com>
2026-04-10 09:58:00 +0800

    复盘

--- STUDY/.leetcode/572.另一棵树的子树.c
+++ STUDY/.leetcode/572.另一棵树的子树.c
@@ -14,23 +14,24 @@
  * };
  */
 typedef struct TreeNode TNode;
-bool isSameTree(TNode* root, TNode* subroot)
+bool isSameTree(TNode* p, TNode* q)
 {
-    if(!root && !subroot) return true;
+    if(!p && !q)
+    return true;
 
-    if(!root || !subroot) return false;
+    if(!p || !q)
+    return false;
 
-    if(root->val != subroot->val) return false;
+    if(p->val != q->val)
+    return false;
 
-    return isSameTree(root->left, subroot->left) && isSameTree(root->right, subroot->right);
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 }
 
 bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot)
 {
     if(!root)
-    {
-        return false;
-    }
+    return false;
 
     if(root->val == subRoot->val && isSameTree(root, subRoot))
     {
@@ -38,9 +39,34 @@ bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot)
     }
 
     return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
-
 }
 // @lc code=end
 
 //一刷：递归的思路还是要加强TvT。本质上是遍历树，找出所有子树与subroot进行比较，最后只要有true表示找到了子树（因为返回是或关系
+// typedef struct TreeNode TNode;
+// bool isSameTree(TNode* root, TNode* subroot)
+// {
+//     if(!root && !subroot) return true;
 
+//     if(!root || !subroot) return false;
+
+//     if(root->val != subroot->val) return false;
+
+//     return isSameTree(root->left, subroot->left) && isSameTree(root->right, subroot->right);
+// }
+
+// bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot)
+// {
+//     if(!root)
+//     {
+//         return false; 
+//     }
+
+//     if(root->val == subRoot->val && isSameTree(root, subRoot))
+//     {
+//         return true;
+//     }
+
+//     return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+
+// }
