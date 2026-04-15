8caaed8e7cf8acf47f03274cf9df8bd61656d858
Hikari <tairitsu_h@qq.com>
2026-04-15 20:35:09 +0800

    练习

--- STUDY/BYTE CLASS/Class.cpp
+++ STUDY/BYTE CLASS/Class.cpp
@@ -1,21 +1,93 @@
+// class A
+// {
+// public:
+//     void Print()
+//     {
+//         cout << "A::Print()" << endl;
+//     }
+
+// private:
+//     int _a;
+// };
+
+// int main()
+// {
+//     A* p = nullptr;
+//     p->Print();
+//     return 0;
+// }
+
 #include<iostream>
 using namespace std;
 
-class A
+class Student
 {
+private:
+    string _name;
+    int _age;
+    int* _scores;
+    int _scoreCount;
+
 public:
-    void Print()
+    Student(const string& name, int age, int s1, int s2, int s3)
     {
-        cout << "A::Print()" << endl;
+        _name = name;
+        _age = age;
+        _scores = (int*)malloc(sizeof(int)*3);
+        _scores[0] = s1;
+        _scores[1] = s2;
+        _scores[2] = s3;
     }
 
-private:
-    int _a;
+    ~Student()
+    {
+        free(_scores);
+        _scores = nullptr;
+    }
+
+    double GetAverage()
+    {
+        double sum = 0;
+        for(int i=0; i<3; i++)
+        {
+            sum += _scores[i];
+        }
+        return sum / 3;
+    }
+
+    void Print()
+    {
+        cout << "姓名：" << _name << endl;
+        cout << "年龄：" << _age << endl;
+        cout << "平均分：" << GetAverage() << endl;
+    }
+
+    void CompareAge(const Student& other)
+    {
+        if(this->_age > other._age)
+        {
+            cout << _name << "的年龄大" << endl;
+        }
+        else if(this->_age == other._age)
+        {
+            cout << "你们的年龄一样大" << endl;
+        }
+        else
+        {
+            cout << other._name << "的年龄大" << endl;
+        }
+    }
 };
 
 int main()
 {
-    A* p = nullptr;
-    p->Print();
+    Student Stu1("Hikari", 18, 150, 100, 150);
+    Student Stu2("Tairitsu", 19, 120, 130, 140);
+
+    Stu1.Print();
+    Stu2.Print();
+
+    Stu1.CompareAge(Stu2);
+
     return 0;
 }
