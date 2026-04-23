cab59f0f68a16e2dbb8e424364a7efa792f323d6
Hikari <tairitsu_h@qq.com>
2026-04-23 19:46:05 +0800

    CMake

---
+++ build/CMakeFiles/String.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/CODE/PROJECT/String/String.cpp" "CMakeFiles/String.dir/String.cpp.obj" "gcc" "CMakeFiles/String.dir/String.cpp.obj.d"
+  "D:/CODE/PROJECT/String/testStr.cpp" "CMakeFiles/String.dir/testStr.cpp.obj" "gcc" "CMakeFiles/String.dir/testStr.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
