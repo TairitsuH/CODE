41f05cba21faed1d65876f0ab2245ffe5ef6e252
Hikari <tairitsu_h@qq.com>
2026-04-23 14:46:11 +0800

    练习

--- STUDY/.leetcode/415.字符串相加.cpp
+++ STUDY/.leetcode/415.字符串相加.cpp
@@ -3,7 +3,6 @@
  *
  * [415] 字符串相加
  */
-
 // @lc code=start
 class Solution {
 public:
@@ -13,28 +12,64 @@ public:
 
         int pos1 = num1.size() - 1;
         int pos2 = num2.size() - 1;
-        int next = 0; //处理进位
-        while(pos1 >= 0 || pos2 >= 0) //都为0时才停止
-        {
-            int ret1 = pos1 >= 0 ? num1[pos1] - '0' : 0;
-            int ret2 = pos2 >= 0 ? num2[pos2] - '0' : 0;
+        int next = 0;
 
+        while(pos1 >= 0 || pos2 >= 0)
+        {
+            int ret1 = pos1 < 0 ? 0 : num1[pos1] - '0';
+            int ret2 = pos2 < 0 ? 0 : num2[pos2] - '0';
             int ret = ret1 + ret2 + next;
+
             next = ret / 10;
             ret %= 10;
-
             str += ret + '0';
+
             pos1--;
             pos2--;
         }
 
-        if(next == 1) str += '1';
+        if(next == 1)
+        {
+            str += '1';
+        }
 
         reverse(str.begin(), str.end());
+
         return str;
     }
 };
+
 // @lc code=end
 
+//二刷：还是不太熟练，感觉没有理解到位，还是在凭借记忆刷题。一些细节一定要自己调试推导一遍！
 //一刷：思路类似高精度加法OvO
 //while的条件一定要深思！区分进循环和出循环，while需要写进循环的条件！
+// class Solution {
+// public:
+//     string addStrings(string num1, string num2)
+//     {
+//         string str;
+
+//         int pos1 = num1.size() - 1;
+//         int pos2 = num2.size() - 1;
+//         int next = 0; //处理进位
+//         while(pos1 >= 0 || pos2 >= 0) //都为0时才停止
+//         {
+//             int ret1 = pos1 >= 0 ? num1[pos1] - '0' : 0;
+//             int ret2 = pos2 >= 0 ? num2[pos2] - '0' : 0;
+
+//             int ret = ret1 + ret2 + next;
+//             next = ret / 10;
+//             ret %= 10;
+
+//             str += ret + '0';
+//             pos1--;
+//             pos2--;
+//         }
+
+//         if(next == 1) str += '1';
+
+//         reverse(str.begin(), str.end());
+//         return str;
+//     }
+// };
