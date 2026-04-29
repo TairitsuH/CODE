80d4e7fe7c8710ed4e3deda4775a7dd5dd1bf2b3
Hikari <tairitsu_h@qq.com>
2026-04-29 09:04:38 +0800

    复盘

--- STUDY/.leetcode/118.杨辉三角.cpp
+++ STUDY/.leetcode/118.杨辉三角.cpp
@@ -9,7 +9,6 @@ public:
     vector<vector<int>> generate(int numRows)
     {
         vector<vector<int>> vv(numRows);
-
         for(size_t i=0; i<numRows; i++)
         {
             vv[i].resize(i+1, 1);
@@ -17,7 +16,7 @@ public:
 
         for(size_t i=2; i<numRows; i++)
         {
-            for(size_t j=1; j<vv[i].size()-1; j++) //注意减1
+            for(size_t j=1; j<vv[i].size()-1; j++)
             {
                 vv[i][j] = vv[i-1][j] + vv[i-1][j-1];
             }
@@ -28,4 +27,27 @@ public:
 };
 // @lc code=end
 
+//二刷；边界条件和扩容还需加强
 //一刷：对于函数的熟悉度还不够，扩容机制也不熟悉，还需巩固
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int numRows)
+//     {
+//         vector<vector<int>> vv(numRows);
+
+//         for(size_t i=0; i<numRows; i++)
+//         {
+//             vv[i].resize(i+1, 1);
+//         }
+
+//         for(size_t i=2; i<numRows; i++)
+//         {
+//             for(size_t j=1; j<vv[i].size()-1; j++) //注意减1
+//             {
+//                 vv[i][j] = vv[i-1][j] + vv[i-1][j-1];
+//             }
+//         }
+
+//         return vv;
+//     }
+// };
