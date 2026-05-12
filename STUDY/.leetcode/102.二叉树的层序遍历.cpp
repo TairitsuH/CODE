24fa9798ecb0b5593db50fda036c5f83ab479e85
Hikari <tairitsu_h@qq.com>
2026-05-12 09:02:12 +0800

    复盘

--- STUDY/.leetcode/102.二叉树的层序遍历.cpp
+++ STUDY/.leetcode/102.二叉树的层序遍历.cpp
@@ -16,13 +16,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// @lc code=end
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root)
     {
         vector<vector<int>> vv;
-        int levelsize = 0;
         queue<TreeNode*> q;
+        int levelsize = 0;
+
         if(root)
         {
             q.push(root);
@@ -35,27 +38,107 @@ public:
             while(levelsize--)
             {
                 v.push_back(q.front()->val);
-
                 if(q.front()->left)
                 {
                     q.push(q.front()->left);
                 }
-
                 if(q.front()->right)
                 {
                     q.push(q.front()->right);
-                }     
+                }
 
                 q.pop();
             }
 
-            levelsize = q.size();
             vv.push_back(v);
+            levelsize = q.size();
         }
 
         return vv;
     }
 };
-// @lc code=end
 
+
+//三刷：重新捋了一遍思路就顺了^_^
+//二刷：熟练度和审题还需提升
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root)
+//     {
+//         vector<vector<int>> vv;
+//         queue<TreeNode*> q;
+//         int levelsize = 0;
+//         if(root)
+//         {
+//             q.push(root);
+//             levelsize = 1;
+//         }
+
+//         while(!q.empty())
+//         {
+//             vector<int> v;
+
+//             while(levelsize--)
+//             {
+//                 v.push_back(q.front()->val);
+
+//                 if(q.front()->left)
+//                 {
+//                     q.push(q.front()->left);
+//                 }
+//                 if(q.front()->right)
+//                 {
+//                     q.push(q.front()->right);
+//                 }
+                
+//                 q.pop();
+//             }
+
+//             levelsize = q.size();
+//             vv.push_back(v);
+//         }
+
+//         return vv;
+//     }
+// };
 //一刷：思路一是一层一层出队列，用while和levelsize记录和控制每一层的节点数，队列记录每一层的节点指针
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root)
+//     {
+//         vector<vector<int>> vv;
+//         int levelsize = 0;
+//         queue<TreeNode*> q;
+//         if(root)
+//         {
+//             q.push(root);
+//             levelsize = 1;
+//         }
+
+//         while(!q.empty())
+//         {
+//             vector<int> v;
+//             while(levelsize--)
+//             {
+//                 v.push_back(q.front()->val);
+
+//                 if(q.front()->left)
+//                 {
+//                     q.push(q.front()->left);
+//                 }
+
+//                 if(q.front()->right)
+//                 {
+//                     q.push(q.front()->right);
+//                 }     
+
+//                 q.pop();
+//             }
+
+//             levelsize = q.size();
+//             vv.push_back(v);
+//         }
+
+//         return vv;
+//     }
+// };
