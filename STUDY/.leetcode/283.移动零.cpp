990bf7387ac393754c3dddd0d61bfffc3ed6fc4b
Hikari <tairitsu_h@qq.com>
2026-05-25 10:30:27 +0800

    复盘

--- STUDY/.leetcode/283.移动零.cpp
+++ STUDY/.leetcode/283.移动零.cpp
@@ -8,22 +8,37 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums)
     {
-        int slow = 0;
-        int fast = 0;
-        while(fast < nums.size())
+        for(int cur=0, dest =-1; cur<nums.size(); cur++)
         {
-            if(nums[fast] != 0)
+            if(nums[cur] != 0)
             {
-                swap(nums[slow], nums[fast]);
-                slow++;
+                dest++;
+                swap(nums[cur], nums[dest]);
             }
-
-            fast++;
-        }
+        }  
     }
 };
 // @lc code=end
+//三刷：dest设置在最后一个非零数的位置
 //二刷：确定好快慢指针的动向
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums)
+//     {
+//         int slow = 0;
+//         int fast = 0;
+//         while(fast < nums.size())
+//         {
+//             if(nums[fast] != 0)
+//             {
+//                 swap(nums[slow], nums[fast]);
+//                 slow++;
+//             }
+
+//             fast++;
+//         }
+//     }
+// };
 //一刷；slow指向的是数组中从左往右第一个0的位置，fast跳过0寻找之后不为0的数并与slow交换
 // class Solution {
 // public:
