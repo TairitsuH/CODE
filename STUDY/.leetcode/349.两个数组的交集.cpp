35ebb0f88649769e9fec241c0ae1ea885a80eaa5
Hikari <tairitsu_h@qq.com>
2026-05-30 09:17:57 +0800

    复盘

--- STUDY/.leetcode/349.两个数组的交集.cpp
+++ STUDY/.leetcode/349.两个数组的交集.cpp
@@ -5,36 +5,71 @@
  */
 
 // @lc code=start
+
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2)
     {
-        vector<int> v;
         set<int> s1(nums1.begin(), nums1.end());
         set<int> s2(nums2.begin(), nums2.end());
 
         auto p1 = s1.begin();
         auto p2 = s2.begin();
+
+        vector<int> ret;
         while(p1 != s1.end() && p2 != s2.end())
         {
-            if(*p1 == *p2)
+            if(*p1 < *p2)
             {
-                v.push_back(*p1);
                 p1++;
-                p2++;
             }
-            else if(*p1 < *p2)
+            else if(*p1 > *p2)
             {
-                p1++;
+                p2++;
             }
             else
             {
+                ret.push_back(*p1);
+                p1++;
                 p2++;
             }
         }
 
-        return v;
+        return ret;
     }
 };
+
 // @lc code=end
+//二刷：去重（set）+设置迭代器，相等则尾插，注意迭代器对应的容器！
 //一刷：先将两个数组去重+排序（set），再用双指针(迭代器类型！)找交集。
+// class Solution {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2)
+//     {
+//         vector<int> v;
+//         set<int> s1(nums1.begin(), nums1.end());
+//         set<int> s2(nums2.begin(), nums2.end());
+
+//         auto p1 = s1.begin();
+//         auto p2 = s2.begin();
+//         while(p1 != s1.end() && p2 != s2.end())
+//         {
+//             if(*p1 == *p2)
+//             {
+//                 v.push_back(*p1);
+//                 p1++;
+//                 p2++;
+//             }
+//             else if(*p1 < *p2)
+//             {
+//                 p1++;
+//             }
+//             else
+//             {
+//                 p2++;
+//             }
+//         }
+
+//         return v;
+//     }
+// };
