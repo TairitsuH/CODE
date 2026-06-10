a005cc5a297ddd87039c4c119b7227e1e1b10d9e
Hikari <tairitsu_h@qq.com>
2026-06-10 14:09:55 +0800

    练习

--- STUDY/.leetcode/371.两整数之和.cpp
+++ STUDY/.leetcode/371.两整数之和.cpp
@@ -11,11 +11,10 @@ public:
     {
         while(b != 0)
         {
-            long long x = a ^ b;
-            long long pos = (a & b) << 1;
-
-            a = x;
-            b = pos;
+            unsigned int sum = a ^ b;
+            unsigned int carry = a & b;
+            a = sum;
+            b = (carry << 1);
         }
 
         return a;
@@ -23,4 +22,21 @@ public:
 };
 // @lc code=end
 
+//二刷：注意carry要左移一位，unsigned int可以保证符号位逻辑左移受保护
 //一刷：a ^ b可以实现无进位相加，a & b可以实现仅进位，两者配合即可计算出结果。
+// class Solution {
+// public:
+//     int getSum(int a, int b)
+//     {
+//         while(b != 0)
+//         {
+//             unsigned int x = a ^ b;
+//             unsigned int pos = (a & b) << 1;
+
+//             a = x;
+//             b = pos;
+//         }
+
+//         return a;
+//     }
+// };
