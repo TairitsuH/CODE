c58c252e78d39c9cb6edb7b0b2c8e60ac4b5bd4f
Hikari <tairitsu_h@qq.com>
2026-06-10 14:58:22 +0800

    练习

---
+++ STUDY/.leetcode/17.19.消失的两个数字.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> missingTwo(vector<int>& nums)
+    {
+        int val = 0;
+        int num = 1;
+        for(auto x : nums)
+        {
+            val ^= x;
+        }
+        for(num=1; num<=nums.size()+2; num++)
+        {
+            val ^= num;
+        }
+        
+        int pos = val & (-val);
+        int a = 0;
+
+        for(auto x : nums)
+        {
+            if(x & pos)
+            {
+                a ^= x;
+            }
+        }
+        for(num=1; num<=nums.size()+2; num++)
+        {
+            if(num & pos)
+            {
+                a ^= num;
+            }
+        }
+
+        val ^= a;
+
+        return {val, a};
+    }
+};
+
+//一刷：思路是全部异或一遍，转化为LC260，然后再根据最后一个1的位置把两个数分到不同区间，最后输出
