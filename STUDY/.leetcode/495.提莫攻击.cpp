a03fc2ee63b8878c5b9bef4f6a96b430a8db8157
Hikari <tairitsu_h@qq.com>
2026-06-10 15:52:05 +0800

    练习

---
+++ STUDY/.leetcode/495.提莫攻击.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=495 lang=cpp
+ *
+ * [495] 提莫攻击
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration)
+    {
+        int sum = 0;
+        int n = timeSeries.size();
+
+        for(int i=0; i<n-1; i++)
+        {
+            if(timeSeries[i+1] - timeSeries[i] >= duration)
+            {
+                sum += duration;
+            }
+            else
+            {
+                sum += timeSeries[i+1] - timeSeries[i];
+            }
+        }
+
+        sum += duration;
+
+        return sum;
+    }
+};
+// @lc code=end
+
+//一刷：时间差大于等于duration不会重叠，小于duration会重叠，注意最后要加上最后一次的时间。
