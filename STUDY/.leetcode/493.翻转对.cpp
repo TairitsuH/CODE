a205cd9884ab3890b0b8595fd2e52546de956973
TairitsuH <tairitsu_h@qq.com>
2026-06-27 12:52:39 +0800

    练习

--- STUDY/.leetcode/493.翻转对.cpp
+++ STUDY/.leetcode/493.翻转对.cpp
@@ -17,14 +17,13 @@ public:
         return Merge_Sort(nums, 0, n - 1);
     }
 
-    //倒序
     int Merge_Sort(vector<int>& nums, int left, int right)
     {
         if(left >= right) return 0;
 
         int mid = (left + right) / 2;
         int ret = 0;
-
+        
         ret += Merge_Sort(nums, left, mid);
         ret += Merge_Sort(nums, mid + 1, right);
 
@@ -32,7 +31,6 @@ public:
         int cur2 = mid + 1;
         int i = 0;
 
-        //计数
         while(cur1 <= mid)
         {
             while(cur2 <= right && nums[cur1] / 2.0 <= nums[cur2])
@@ -40,24 +38,27 @@ public:
                 cur2++;
             }
 
-            if(cur2 > right) break;
+            if(cur2 > right)
+            {
+                break;
+            }
 
             ret += right - cur2 + 1;
             cur1++;
         }
 
-        //排序
         cur1 = left;
         cur2 = mid + 1;
+
         while(cur1 <= mid && cur2 <= right)
         {
             tmp[i++] = nums[cur1] >= nums[cur2] ? nums[cur1++] : nums[cur2++];
         }
-        
-        while(cur1 <= mid) tmp[i++] = nums[cur1++];
-        while(cur2 <= right) tmp[i++] = nums[cur2++];
 
-        //复原
+        while(cur1 <= mid) tmp[i++] = nums[cur1++];
+
+        while(cur2<= right) tmp[i++] = nums[cur2++];
+
         for(int j=left; j<=right; j++)
         {
             nums[j] = tmp[j - left];
@@ -66,7 +67,71 @@ public:
         return ret;
     }
 };
+
 // @lc code=end
+//三刷：一遍过~
+//二刷：思路很顺
+// class Solution {
+// public:
+//     vector<int> tmp;
+
+//     int reversePairs(vector<int>& nums)
+//     {
+//         int n = nums.size();
+//         tmp.resize(n);
+
+//         return Merge_Sort(nums, 0, n - 1);
+//     }
+
+//     //倒序
+//     int Merge_Sort(vector<int>& nums, int left, int right)
+//     {
+//         if(left >= right) return 0;
+
+//         int mid = (left + right) / 2;
+//         int ret = 0;
+
+//         ret += Merge_Sort(nums, left, mid);
+//         ret += Merge_Sort(nums, mid + 1, right);
+
+//         int cur1 = left;
+//         int cur2 = mid + 1;
+//         int i = 0;
+
+//         //计数
+//         while(cur1 <= mid)
+//         {
+//             while(cur2 <= right && nums[cur1] / 2.0 <= nums[cur2])
+//             {
+//                 cur2++;
+//             }
+
+//             if(cur2 > right) break;
+
+//             ret += right - cur2 + 1;
+//             cur1++;
+//         }
+
+//         //排序
+//         cur1 = left;
+//         cur2 = mid + 1;
+//         while(cur1 <= mid && cur2 <= right)
+//         {
+//             tmp[i++] = nums[cur1] >= nums[cur2] ? nums[cur1++] : nums[cur2++];
+//         }
+        
+//         while(cur1 <= mid) tmp[i++] = nums[cur1++];
+//         while(cur2 <= right) tmp[i++] = nums[cur2++];
+
+//         //复原
+//         for(int j=left; j<=right; j++)
+//         {
+//             nums[j] = tmp[j - left];
+//         }
+
+//         return ret;
+//     }
+// };
 //一刷：思路要灵活变通，对于不能一次性解决计数和排序的问题，就拆分步骤
 // class Solution {
 // public:
