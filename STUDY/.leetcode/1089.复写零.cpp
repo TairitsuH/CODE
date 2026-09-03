34320909cef7bcfdefaa07a37c2d2d95d3abb1cb
TairitsuH <tairitsu_h@qq.com>
2026-09-03 21:11:22 +0800

    复盘+新解法

--- STUDY/.leetcode/1089.复写零.cpp
+++ STUDY/.leetcode/1089.复写零.cpp
@@ -4,63 +4,95 @@
  * [1089] 复写零
  */
 // @lc code=start
-
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr)
     {
         int n = arr.size();
-        int cur = 0;
-        int dest = -1;
-        while(cur < n)
+        int cnt = 0;
+        int i = 0;
+        for(i=0; i<n; i++)
         {
-            if(arr[cur] == 0)
+            if(arr[i] == 0) cnt += 2;
+            else ++cnt;
+            if(cnt >= n) break;
+        }
+
+        for(int left=i, right=n-1; left >= 0; left--)
+        {
+            if(left == i && cnt > n)
             {
-                dest += 2;
+                arr[right--] = 0;
+                continue;
+            }
+            if(arr[left] == 0)
+            {
+                arr[right] = 0;
+                arr[--right] = 0;
             }
             else
             {
-                dest++;
+                arr[right] = arr[left];
             }
-
-            if(dest >= n - 1)
-            {
-                break;
-            }
-
-            cur++;
-        }
-
-        if(dest > n - 1)
-        {
-            arr[n - 1] = 0;
-            dest -= 2;
-            cur--;
-        }
-
-        while(dest >= 0)
-        {
-            if(arr[cur] == 0)
-            {
-                arr[dest] = 0;
-                arr[dest - 1] = 0;
-                dest -= 2;
-            }
-            else
-            {
-                arr[dest] = arr[cur];
-                dest--;
-            }
-
-            cur--;
+            --right;
         }
     }
 };
-
-
 // @lc code=end
 
+//三刷：先预计算所需空间，再从后往前使用双指针。注意特殊情况
 //二刷：试着不用插入的方式，边界条件还需要注意！自己画图+模拟一遍
+// class Solution {
+// public:
+//     void duplicateZeros(vector<int>& arr)
+//     {
+//         int n = arr.size();
+//         int cur = 0;
+//         int dest = -1;
+//         while(cur < n)
+//         {
+//             if(arr[cur] == 0)
+//             {
+//                 dest += 2;
+//             }
+//             else
+//             {
+//                 dest++;
+//             }
+
+//             if(dest >= n - 1)
+//             {
+//                 break;
+//             }
+
+//             cur++;
+//         }
+
+//         if(dest > n - 1)
+//         {
+//             arr[n - 1] = 0;
+//             dest -= 2;
+//             cur--;
+//         }
+
+//         while(dest >= 0)
+//         {
+//             if(arr[cur] == 0)
+//             {
+//                 arr[dest] = 0;
+//                 arr[dest - 1] = 0;
+//                 dest -= 2;
+//             }
+//             else
+//             {
+//                 arr[dest] = arr[cur];
+//                 dest--;
+//             }
+
+//             cur--;
+//         }
+//     }
+// };
 //一刷：注意迭代器的失效问题！循环和内部都涉及了i的修改
 // class Solution {
 // public:
