ebae4afc48d28a9aac38fd4fa9d7054dfb92878b
TairitsuH <tairitsu_h@qq.com>
2026-09-06 18:37:36 +0800

    lc611

--- STUDY/.leetcode/611.有效三角形的个数.cpp
+++ STUDY/.leetcode/611.有效三角形的个数.cpp
@@ -9,22 +9,23 @@ public:
     int triangleNumber(vector<int>& nums)
     {
         sort(nums.begin(), nums.end());
+        int n = nums.size();
         int cnt = 0;
 
-        for(int i=nums.size()-1; i>=2; i--)
+        for(int i=n-1; i>=2; i--)
         {
             int left = 0;
-            int right = i-1;
+            int right = i - 1;
             while(left < right)
             {
                 if(nums[left] + nums[right] > nums[i])
                 {
                     cnt += right - left;
-                    right--;
+                    --right;
                 }
                 else
                 {
-                    left++;
+                    ++left;
                 }
             }
         }
@@ -32,9 +33,38 @@ public:
         return cnt;
     }
 };
-
 // @lc code=end
+//三刷：做题有三层境界，第一层是懂得使用暴力，第二层是懂得使用更优解法，第三层是学会如何由暴力解法优化至优解
 //二刷：排序->单调性+双指针，时间复杂度O(N*N)，注意循环是自减！
+// class Solution {
+// public:
+//     int triangleNumber(vector<int>& nums)
+//     {
+//         sort(nums.begin(), nums.end());
+//         int cnt = 0;
+
+//         for(int i=nums.size()-1; i>=2; i--)
+//         {
+//             int left = 0;
+//             int right = i-1;
+//             while(left < right)
+//             {
+//                 if(nums[left] + nums[right] > nums[i])
+//                 {
+//                     cnt += right - left;
+//                     right--;
+//                 }
+//                 else
+//                 {
+//                     left++;
+//                 }
+//             }
+//         }
+
+//         return cnt;
+//     }
+// };
+
 //一刷：思路是先排序，然后顺序挑选两个数和后面的数进行比较，若大于则计数，小于则重新循环
 //时间复杂度O(N*logN + N*N*N)
 // class Solution {
