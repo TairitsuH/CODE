0e8c59b01fd128b0a30326819ea7aa56b21da3a5
TairitsuH <tairitsu_h@qq.com>
2026-09-10 19:37:41 +0800

    lc3

--- STUDY/.leetcode/3.无重复字符的最长子串.cpp
+++ STUDY/.leetcode/3.无重复字符的最长子串.cpp
@@ -5,35 +5,82 @@
  */
 
 // @lc code=start
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s)
     {
-        int left = 0;
-        int right = 0;
+        vector<int> arr(128, 0);
+        int len = 0;
         int n = s.size();
-        int maxlen = 0;
-        vector<int> hash(128, 0); //128个字符，用数组模拟哈希表
 
-        while(right < n)
+        for(int left=0, right=0; right<n; right++)
         {
-            hash[s[right]]++;
-
-            while(hash[s[right]] == 2) //加1在前判断在后则为2
+            ++arr[s[right]];
+            while(arr[s[right]] > 1)
             {
-                hash[s[left++]]--;
+                --arr[s[left]];
+                ++left;
             }
 
-            maxlen = max(maxlen, right - left + 1);
-            right++;
+            len = max(len, right - left + 1);
         }
 
-        return maxlen;
+        return len;
     }
 };
-
 // @lc code=end
+//四刷：数组+滑动窗口
+//三刷：哈希+滑动窗口，对于哈希下标和接口还是不熟悉
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s)
+//     {
+//         int len = 0;
+//         int n = s.size();
+//         unordered_map<char, int> hash;
+
+//         for(int left=0, right=0; right<n; right++)
+//         {
+//             ++hash[s[right]];
+//             while(hash[s[right]] > 1)
+//             {
+//                 --hash[s[left]];
+//                 ++left;
+//             }
+
+//             len = max(len, right - left + 1);
+//         }
+//         return len;
+//     }
+// };
 //二刷：滑动窗口，子串在遇到重复元素后无需回退，仅需跳过重复元素即可
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s)
+//     {
+//         int left = 0;
+//         int right = 0;
+//         int n = s.size();
+//         int maxlen = 0;
+//         vector<int> hash(128, 0); //128个字符，用数组模拟哈希表
+
+//         while(right < n)
+//         {
+//             hash[s[right]]++;
+
+//             while(hash[s[right]] == 2) //加1在前判断在后则为2
+//             {
+//                 hash[s[left++]]--;
+//             }
+
+//             maxlen = max(maxlen, right - left + 1);
+//             right++;
+//         }
+
+//         return maxlen;
+//     }
+// };
 //一刷：暴力，根据128个字符可以创建一个128大小的整型数组作为哈希表，用于查重。逐位遍历找最大字串
 // class Solution {
 // public:
