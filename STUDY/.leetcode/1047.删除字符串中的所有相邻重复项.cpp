d036fdf445d7b2f3e83e5f0568403e291e016013
TairitsuH <tairitsu_h@qq.com>
2026-09-10 20:20:41 +0800

    lc1047

---
+++ STUDY/.leetcode/1047.删除字符串中的所有相邻重复项.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1047 lang=cpp
+ *
+ * [1047] 删除字符串中的所有相邻重复项
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string removeDuplicates(string s)
+    {
+        string ret = "";
+        for(auto c : s)
+        {
+            if(ret.size() > 0 && c == ret.back())
+            {
+                ret.pop_back();
+            }
+            else
+            {
+                ret += c;
+            }
+        }    
+
+        return ret;
+    }
+};
+// @lc code=end
+//一刷：用数组模拟栈的后进先出，从而实现消除重复项
+
