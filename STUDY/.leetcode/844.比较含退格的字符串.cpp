4d242ea4326c05b07720b533c8fae456b7962487
TairitsuH <tairitsu_h@qq.com>
2026-09-10 20:27:30 +0800

    lc844

--- STUDY/.leetcode/844.比较含退格的字符串.cpp
+++ STUDY/.leetcode/844.比较含退格的字符串.cpp
@@ -9,41 +9,74 @@ class Solution {
 public:
     bool backspaceCompare(string s, string t)
     {
-        int len1 = s.size();
-        int len2 = t.size();
-        int s1 = 0, s2 = 0;
-    
-        for(int f=0; f<len1; f++)
+        string s1, t1;
+        for(auto c : s)
         {
-            if(s[f] != '#')
+            if(s1.size() && c == '#')
             {
-                s[s1] = s[f];
-                s1++;
+                s1.pop_back();
             }
-            else
+            else if(c != '#')
             {
-                if(s1 > 0) s1--;
+                s1 += c;
             }
         }
 
-        for(int f=0; f<len2; f++)
+        for(auto c : t)
         {
-            if(t[f] != '#')
+            if(t1.size() && c == '#')
             {
-                t[s2] = t[f];
-                s2++;
+                t1.pop_back();
             }
-            else
+            else if(c != '#')
             {
-                if(s2 > 0) s2--;
+                t1 += c;
             }
         }
-        s.resize(s1), t.resize(s2);
-        if(s == t) return true;
-        return false;
-        
+
+        return s1 == t1;
     }
 };
 // @lc code=end
 
-//一刷：思路乱了，看了评论区题解，遇到#则fast后移，slow--（slow大于0）
+//二刷：栈模拟，遇到#且非空则出栈，非#则入栈
+//一刷：双指针。思路乱了，看了评论区题解，遇到#则fast后移，slow--（slow大于0）
+// class Solution {
+// public:
+//     bool backspaceCompare(string s, string t)
+//     {
+//         int len1 = s.size();
+//         int len2 = t.size();
+//         int s1 = 0, s2 = 0;
+    
+//         for(int f=0; f<len1; f++)
+//         {
+//             if(s[f] != '#')
+//             {
+//                 s[s1] = s[f];
+//                 s1++;
+//             }
+//             else
+//             {
+//                 if(s1 > 0) s1--;
+//             }
+//         }
+
+//         for(int f=0; f<len2; f++)
+//         {
+//             if(t[f] != '#')
+//             {
+//                 t[s2] = t[f];
+//                 s2++;
+//             }
+//             else
+//             {
+//                 if(s2 > 0) s2--;
+//             }
+//         }
+//         s.resize(s1), t.resize(s2);
+//         if(s == t) return true;
+//         return false;
+        
+//     }
+// };
