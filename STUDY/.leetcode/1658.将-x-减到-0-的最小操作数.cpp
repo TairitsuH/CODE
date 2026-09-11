20f015070cea663ac7e8cdba6f9b07bcda9e1610
TairitsuH <tairitsu_h@qq.com>
2026-09-11 13:08:40 +0800

    lc1658

--- STUDY/.leetcode/1658.将-x-减到-0-的最小操作数.cpp
+++ STUDY/.leetcode/1658.将-x-减到-0-的最小操作数.cpp
@@ -9,40 +9,76 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x)
     {
-        int left = 0, right = 0, minlen = INT_MAX;
-        long long sum = 0;
-        int arrsum = 0;
         int n = nums.size();
-
-        // 求和
-        for (auto i : nums)
-        {
-            arrsum += i;
-        }
-        long long key = arrsum - x;
-        if(key < 0) return -1; //所有的数加起来都比x小，则不成立（滑动窗口仅支持大于零的元素）
-
-        while (right < n)
-        {
-            sum += nums[right];
-            
-            while (sum > key) //注意>的判断要在==前
-            {
-                sum -= nums[left++];
-            }
         
-            if (sum == key)
+        int sum = 0;
+        for(auto n : nums)
+        {
+            sum += n;
+        }
+        if(x > sum) return -1;
+
+        int target = sum - x;
+        int sumarr = 0;
+        int len = -1;
+        for(int left=0, right=0; right < n; right++)
+        {
+            sumarr += nums[right];
+            while(sumarr > target)
             {
-                int len = n - (right - left + 1);
-                minlen = min(minlen, len);
+                sumarr -= nums[left++];
             }
 
-            right++;
+            if(sumarr == target)
+            {
+                len = max(len, right - left + 1);
+            }
         }
 
-        return minlen == INT_MAX ? -1 : minlen;
+        if(len == -1) return -1;
+        return n - len;
+
     }
 };
 
 // @lc code=end
+//二刷：从数组两端很难入手，但是可以逆向思维从中间入手，最后的返回值记得分情况
 //一刷：正难则反，滑动窗口。找中间和为arrsum-x的最大区间，剩下的元素即为和为x的最小个数
+// class Solution {
+// public:
+//     int minOperations(vector<int>& nums, int x)
+//     {
+//         int left = 0, right = 0, minlen = INT_MAX;
+//         long long sum = 0;
+//         int arrsum = 0;
+//         int n = nums.size();
+
+//         // 求和
+//         for (auto i : nums)
+//         {
+//             arrsum += i;
+//         }
+//         long long key = arrsum - x;
+//         if(key < 0) return -1; //所有的数加起来都比x小，则不成立（滑动窗口仅支持大于零的元素）
+
+//         while (right < n)
+//         {
+//             sum += nums[right];
+            
+//             while (sum > key) //注意>的判断要在==前
+//             {
+//                 sum -= nums[left++];
+//             }
+        
+//             if (sum == key)
+//             {
+//                 int len = n - (right - left + 1);
+//                 minlen = min(minlen, len);
+//             }
+
+//             right++;
+//         }
+
+//         return minlen == INT_MAX ? -1 : minlen;
+//     }
+// };
