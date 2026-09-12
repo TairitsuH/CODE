a10e223695b4c22e9c23ba498bd62d0ef646207a
TairitsuH <tairitsu_h@qq.com>
2026-09-12 17:36:42 +0800

    lc227

--- STUDY/.leetcode/227.基本计算器-ii.cpp
+++ STUDY/.leetcode/227.基本计算器-ii.cpp
@@ -8,30 +8,42 @@ class Solution {
 public:
     int calculate(string s)
     {
-        stack<int> st;
-        int n = s.size();
         int i = 0;
+        int n = s.size();
         char op = '+';
+        vector<int> v;
         while(i < n)
         {
-            if(s[i] == ' ')
+            //数字
+            if(s[i] >= '0' && s[i] <= '9')
             {
-                ++i;
-            }
-            else if(s[i] >= '0' && s[i] <= '9')
-            {
-                //先把数字提取出来
                 int tmp = 0;
-                while(i < n && s[i] >= '0' && s[i] <= '9') //循环内部已经把i移到下一个位置了，不需要再动i
+                while(i < n && s[i] >='0' && s[i] <= '9')
                 {
                     tmp *= 10;
                     tmp += s[i] - '0';
                     ++i;
                 }
-                if(op == '+') st.push(tmp);
-                else if(op == '-') st.push(-tmp);
-                if(op == '*') st.top() *= tmp;
-                if(op == '/') st.top() /= tmp;
+                if(op == '+')
+                {
+                    v.emplace_back(tmp);
+                }
+                else if(op == '-')
+                {
+                    v.emplace_back(-tmp);
+                }
+                else if(op == '*')
+                {
+                    v.back() *= tmp;
+                }
+                else
+                {
+                    v.back() /= tmp;
+                }
+            }
+            else if(s[i] == ' ')
+            {
+                ++i;
             }
             else
             {
@@ -41,15 +53,60 @@ public:
         }
 
         int ret = 0;
-        while(!st.empty())
+        for(auto x : v)
         {
-            ret += st.top();
-            st.pop();
+            ret += x;
         }
 
         return ret;
     }
 };
 // @lc code=end
-
+//二刷：数组模拟栈，最后的累加更简洁。分类讨论清晰了很多
 //一刷：栈模拟题，模拟计算过程。注意分类讨论！考虑最后一个数的处理情况，而不是单纯移动指针。有点难度但不多TvT多练习就好
+// class Solution {
+// public:
+//     int calculate(string s)
+//     {
+//         stack<int> st;
+//         int n = s.size();
+//         int i = 0;
+//         char op = '+';
+//         while(i < n)
+//         {
+//             if(s[i] == ' ')
+//             {
+//                 ++i;
+//             }
+//             else if(s[i] >= '0' && s[i] <= '9')
+//             {
+//                 //先把数字提取出来
+//                 int tmp = 0;
+//                 while(i < n && s[i] >= '0' && s[i] <= '9') //循环内部已经把i移到下一个位置了，不需要再动i
+//                 {
+//                     tmp *= 10;
+//                     tmp += s[i] - '0';
+//                     ++i;
+//                 }
+//                 if(op == '+') st.push(tmp);
+//                 else if(op == '-') st.push(-tmp);
+//                 if(op == '*') st.top() *= tmp;
+//                 if(op == '/') st.top() /= tmp;
+//             }
+//             else
+//             {
+//                 op = s[i];
+//                 ++i;
+//             }
+//         }
+
+//         int ret = 0;
+//         while(!st.empty())
+//         {
+//             ret += st.top();
+//             st.pop();
+//         }
+
+//         return ret;
+//     }
+// };
