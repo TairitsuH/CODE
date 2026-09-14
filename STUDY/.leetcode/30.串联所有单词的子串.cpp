1edcb206f5d6913c44756ec0d1687b716dfff57d
TairitsuH <tairitsu_h@qq.com>
2026-09-14 20:32:56 +0800

    lc30

--- STUDY/.leetcode/30.串联所有单词的子串.cpp
+++ STUDY/.leetcode/30.串联所有单词的子串.cpp
@@ -10,37 +10,38 @@ public:
     vector<int> findSubstring(string s, vector<string>& words)
     {
         vector<int> ret;
-        unordered_map<string, int> hw;
-        for(auto& str : words)
-        {
-            ++hw[str];
-        }
-
         int n = s.size();
         int m = words.size();
         int len = words[0].size();
-
-        for(int i=0; i<len; ++i)
+        unordered_map<string, int> hw;
+        for(auto& w : words)
         {
-            int count = 0;
+            ++hw[w];
+        }
+
+        for(int i=0; i<len; i++)
+        {
             unordered_map<string, int> hs;
+            int count = 0;
             for(int left=i, right=i; right<=n-len; right+=len)
-            {  
-                //进窗口
+            {
+                //进
                 string in = s.substr(right, len);
                 ++hs[in];
                 if(hw.count(in) && hs[in] <= hw[in]) ++count;
 
-                //出窗口
+
+                //判断+出
                 while(right - left + 1 > m * len)
                 {
                     string out = s.substr(left, len);
-                    if(hw.count(out) && hs[out] <= hw[out]) --count;
+                    if(hs[out] <= hw[out]) --count;
                     --hs[out];
+
                     left += len;
                 }
 
-                //更新结果
+                //更新
                 if(count == m) ret.push_back(left);
             }
         }
@@ -50,7 +51,51 @@ public:
 };
 // @lc code=end
 
+//三刷：还差一点细节需要注重处理：进窗口是从right开始而不是left，否则会死循环；进窗口后记得更新结果；循环的结束条件一定要自己模拟
 //二刷：理解了思路，但是好多细节！好多坑！比如对于字符串的起始位置要多一层for循环来遍历，另外接口的使用也不太熟练
+// class Solution {
+// public:
+//     vector<int> findSubstring(string s, vector<string>& words)
+//     {
+//         vector<int> ret;
+//         unordered_map<string, int> hw;
+//         for(auto& str : words)
+//         {
+//             ++hw[str];
+//         }
+
+//         int n = s.size();
+//         int m = words.size();
+//         int len = words[0].size();
+
+//         for(int i=0; i<len; ++i)
+//         {
+//             int count = 0;
+//             unordered_map<string, int> hs;
+//             for(int left=i, right=i; right<=n-len; right+=len)
+//             {  
+//                 //进窗口
+//                 string in = s.substr(right, len);
+//                 ++hs[in];
+//                 if(hw.count(in) && hs[in] <= hw[in]) ++count;
+
+//                 //出窗口
+//                 while(right - left + 1 > m * len)
+//                 {
+//                     string out = s.substr(left, len);
+//                     if(hw.count(out) && hs[out] <= hw[out]) --count;
+//                     --hs[out];
+//                     left += len;
+//                 }
+
+//                 //更新结果
+//                 if(count == m) ret.push_back(left);
+//             }
+//         }
+
+//         return ret;
+//     }
+// };
 //一刷：理解了思路但很多细节还需要打磨，一不小心就会错，不能脱离题解独立实现。
 // class Solution {
 // public:
