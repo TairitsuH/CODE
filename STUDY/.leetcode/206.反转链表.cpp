3c1e762b7c67dfe3c466d9ecab807a58c130a7a9
TairitsuH <tairitsu_h@qq.com>
2026-09-15 21:32:15 +0800

    lc递归

---
+++ STUDY/.leetcode/206.反转链表.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        if(head == nullptr || head->next == nullptr) return head;
+
+        //逆置
+        ListNode* newhead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        
+        return newhead;
+    }
+};
+// @lc code=end
+
+//递归方式，最后需要返回链表头节点，因此需要传递至递归上层；另外注意指针和节点的区别
--- STUDY/.leetcode/21.合并两个有序链表.cpp
+++ STUDY/.leetcode/21.合并两个有序链表.cpp
@@ -8,10 +8,48 @@
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
- *     struct ListNode *next;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+    {
+        if(list1 == nullptr) return list2;
+        else if(list2 == nullptr) return list1;
+        
+        if(list1->val < list2->val)
+        {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else
+        {
+            list2->next = mergeTwoLists(list2->next, list1);
+            return list2;
+        }
+    }
+};
 
+
+// @lc code=end
+//三刷：递归思路，找重复子问题（取出两个节点中较小的一个）->
+//单独子问题分析（取出节点->拼接节点->最后返回头指针）->
+//递归出口（其中一个走到空，返回另一个）（其实可以不用另开函数）
+//二刷：设置虚拟头结点（哨兵位），减少了重复代码（养成好习惯，记得free！
 typedef struct ListNode LTNode;
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
 {
@@ -58,11 +96,6 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
     return ret;
    
 }
-
-
-// @lc code=end
-
-//二刷：设置虚拟头结点（哨兵位），减少了重复代码（养成好习惯，记得free！）
 //一刷：在原链表上操作很复杂，故直接新建链表。
 //很多需要注意的细节，比如在合并前需要对数组判空，设置两个指针分别指向两个链表的开头，再新建两个指针指向新建链表的头尾。
 // typedef struct ListNode LTNode;
