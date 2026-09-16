c3b31785f6a522f864bb2ca4f280749f0e98337b
TairitsuH <tairitsu_h@qq.com>
2026-09-16 21:59:03 +0800

    lc

--- STUDY/.leetcode/21.合并两个有序链表.cpp
+++ STUDY/.leetcode/21.合并两个有序链表.cpp
@@ -14,16 +14,30 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+    {
+        if(list1 == nullptr) return list2;
+        else if(list2 == nullptr) return list1;
+
+        if(list1->val < list2->val)
+        {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else
+        {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+};
+// @lc code=end
+//四刷：递归，注意走到头时返回另一个链表的头节点
+//三刷：递归思路，找重复子问题（取出两个节点中较小的一个）->
+//单独子问题分析（取出节点->拼接节点->最后返回头指针）->
+//递归出口（其中一个走到空，返回另一个）（其实可以不用另开函数）
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
@@ -44,11 +58,6 @@ public:
     }
 };
 
-
-// @lc code=end
-//三刷：递归思路，找重复子问题（取出两个节点中较小的一个）->
-//单独子问题分析（取出节点->拼接节点->最后返回头指针）->
-//递归出口（其中一个走到空，返回另一个）（其实可以不用另开函数）
 //二刷：设置虚拟头结点（哨兵位），减少了重复代码（养成好习惯，记得free！
 typedef struct ListNode LTNode;
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
---
+++ STUDY/.leetcode/50.pow-x-n.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=cpp
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double pow(double x, long long n)
+    {
+        if(n == 0) return 1;
+
+        double getx = pow(x, n / 2);
+
+        if(n % 2 == 0)
+        {
+            return getx * getx;
+        }
+        else
+        {
+            return getx * getx * x;
+        }
+    }
+
+    double myPow(double x, int n)
+    {
+        if(n < 0) x = 1 / x;
+        return pow(x, (long long)n);
+    }
+};
+// @lc code=end
+//一刷：快速幂（递归），根据指数的特点快速相乘。注意细节：1.n可能为负数 2.n过于小时需用longlong存
+
