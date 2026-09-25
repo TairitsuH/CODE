614f4c152c8727455ef29dd4a994001979afad46
TairitsuH <tairitsu_h@qq.com>
2026-09-25 22:46:00 +0800

    lc递归括号二刷

--- STUDY/.leetcode/20.有效的括号.cpp
+++ STUDY/.leetcode/20.有效的括号.cpp
@@ -9,6 +9,49 @@ class Solution {
 public:
     vector<string> ret;
     string path;
+
+    void dfs(int left, int right, int n)
+    {
+        if(left == n && right == n)
+        {
+            ret.push_back(path);
+            return;
+        }
+
+        if(left > right)
+        {
+            if(left < n)
+            {
+                path += '(';
+                dfs(left + 1, right, n);
+                path.pop_back();
+            }
+            
+            path += ')';
+            dfs(left, right + 1, n);
+            path.pop_back();
+        }
+        else if(left == right)
+        {
+            path += '(';
+            dfs(left + 1, right, n);
+            path.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n)
+    {
+        dfs(0, 0, n);
+        return ret;
+    }
+};
+// @lc code=end
+//二刷：递归，分类讨论，ret和path作为全局变量，注意left>right内部的分类讨论
+//一刷：递归，使用left，right作为函数参数记录左/右括号数量
+class Solution {
+public:
+    vector<string> ret;
+    string path;
     void dfs(int left, int right, int n)
     {
         if(left == n && right == n)
@@ -44,7 +87,5 @@ public:
         return ret;
     }
 };
-// @lc code=end
-//一刷：递归，使用left，right作为函数参数记录左/右括号数量
 //有效括号：1.左括号数量==右括号数量 2.从头开始的子串，左括号数>=右括号数
 
